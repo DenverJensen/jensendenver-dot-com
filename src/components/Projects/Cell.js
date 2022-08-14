42dff66ae942459d4e95a241ffd60d83eb2ea3c8
DenverJensen <jensendenver@gmail.com>
2022-08-14 17:13:50 -0600

    project udpate

--- src/components/Projects/Cell.js
+++ src/components/Projects/Cell.js
@@ -3,30 +3,41 @@ import PropTypes from 'prop-types';
 import dayjs from 'dayjs';
 
 const Cell = ({ data }) => (
-  <div className="cell-container">
-    <article className="mini-post">
-      <header>
-        <h3><a href={data.link}>{data.title}</a></h3>
-        <time className="published">{dayjs(data.date).format('MMMM, YYYY')}</time>
-      </header>
-      <a href={data.link} className="image">
-        <img src={`${process.env.PUBLIC_URL}${data.image}`} alt={data.title} />
-      </a>
-      <div className="description">
-        <p>{data.desc}</p>
-      </div>
-    </article>
-  </div>
+    <div className="cell-container">
+        <article className="mini-post">
+            <header>
+                <h3>
+                    <a href={data.link}>{data.title}</a>
+                </h3>
+                <time className="published">
+                    {dayjs(data.date).format('MMMM, YYYY')}
+                </time>
+            </header>
+            <a href={data.link} className="image">
+                <img
+                  src={`${process.env.PUBLIC_URL}${data.image}`}
+                  alt={data.title}
+                />
+            </a>
+            <div className="description">
+                <p>
+                    {data.desc}
+                    <br /> <br /> You can find more documentation <a href="https://github.com/DenverJensen/hangman"> here </a>
+                </p>
+            </div>
+        </article>
+    </div>
 );
 
 Cell.propTypes = {
-  data: PropTypes.shape({
-    title: PropTypes.string.isRequired,
-    link: PropTypes.string,
-    image: PropTypes.string.isRequired,
-    date: PropTypes.string.isRequired,
-    desc: PropTypes.string.isRequired,
-  }).isRequired,
+    data: PropTypes.shape({
+        title: PropTypes.string.isRequired,
+        link: PropTypes.string,
+        link2: PropTypes.string,
+        image: PropTypes.string.isRequired,
+        date: PropTypes.string.isRequired,
+        desc: PropTypes.string.isRequired,
+    }).isRequired,
 };
 
 export default Cell;
--- src/data/projects.js
+++ src/data/projects.js
@@ -1,48 +1,52 @@
 // TODO Add a couple lines about each project
 const data = [
-  // {
-  //   title: 'Nearest Dollar',
-  //   subtitle: '2015 BVP Hackathon',
-  //   image: '/images/projects/nearestdollar.jpg',
-  //   date: '2015-11-20',
-  //   desc:
-  //     'Built for a social impact hackathon. '
-  //     + 'NearestDollar connected to your bank accounts, credit cards, '
-  //     + 'or debit cards and rounded up your purchases to donate the balance to '
-  //     + 'the charity of your choice.',
-  // },
-  // {
-  //   title: 'Harvest',
-  //   subtitle: 'Won 3rd. place in 2015 Techcrunch Disrupt SF Hackathon',
-  //   link: 'https://devpost.com/software/harvest',
-  //   image: '/images/projects/harvest.jpg',
-  //   date: '2015-09-20',
-  //   desc:
-  //     'Won ~ $7000 in prizes for an advanced, low cost monitoring solution '
-  //     + 'for crops. Harvest was designed to catch irrigation leaks, overwatering, '
-  //     + 'and nutrient deficiencies at an affordable price for the developing world.',
-  // },
-  // {
-  //   title: 'Space Potato',
-  //   subtitle: 'A kickstarter funded potato powered weather balloon.',
-  //   link: 'http://www.spacepotato.org',
-  //   image: '/images/projects/spacepotato.jpg',
-  //   date: '2015-06-28',
-  //   desc:
-  //     'Launched a potato battery powered weather balloon with two cameras '
-  //     + 'and gps transponder. Resulting photos were published in a coffee table book. '
-  //     + 'You can email me for a copy.',
-  // },
-  // {
-  //   title: 'Cat Detector',
-  //   subtitle: 'A convolutional neural network to classify cats! (and dogs)',
-  //   image: '/images/projects/catdetector.jpg',
-  //   date: '2015-05-15',
-  //   desc:
-  //     'Trained a convolutional neural network to classify between ~ 80 cats breeds. '
-  //     + 'Over 60,000 cats were classified before server bills made the project too expensive '
-  //     + 'to continue hosting.',
-  // },
+    {
+        title: 'Hangman Game (PHP/MySQL)',
+        subtitle: 'CS 3750 Software Engineering II',
+        image: '/images/projects/hangman2.JPG',
+        link: 'https://denverjensen.epizy.com/hangman',
+        date: '2022-06-20',
+        desc:
+            'A simple PHP hangman game for Software Engineering II.'
+            + '\nHangman.php is a standalone application that can be viewed at https://denverjensen.epizy.com/hangman'
+            + 'The application includes a full login system with salted and encrypted password authentication (SHA-256) and strong security features'
+            + 'SSL Certification is setup on the site, but my certificate may be expired at the time of viewing the project.'
+            + 'Upon login, the game initializes and selects a word from a list of pre-screened commonly used words in the english language between 5 and 9 letters'
+            + 'Users are given 12 "strikes" or "lives" before the game is ended'
+
+            + 'upon login, the application allows you to guess letters until you get 11 strikes.',
+    },
+    //   {
+    //     title: 'Harvest',
+    //     subtitle: 'Won 3rd. place in 2015 Techcrunch Disrupt SF Hackathon',
+    //     link: 'https://devpost.com/software/harvest',
+    //     image: '/images/projects/harvest.jpg',
+    //     date: '2015-09-20',
+    //     desc:
+    //       'Won ~ $7000 in prizes for an advanced, low cost monitoring solution '
+    //       + 'for crops. Harvest was designed to catch irrigation leaks, overwatering, '
+    //       + 'and nutrient deficiencies at an affordable price for the developing world.',
+    //   },
+    //   {
+    //     title: 'Space Potato',
+    //     subtitle: 'A kickstarter funded potato powered weather balloon.',
+    //     link: 'http://www.spacepotato.org',
+    //     image: '/images/projects/spacepotato.jpg',
+    //     date: '2015-06-28',
+    //     desc:
+    //       'Launched a potato battery powered weather balloon with two cameras '
+    //       + 'and gps transponder. Resulting photos were published in a coffee table book. '
+    //       + 'You can email me for a copy.',
+    //   },
+    //   {
+    //     title: 'Cat Detector',
+    //     subtitle: 'A convolutional neural network to classify cats! (and dogs)',
+    //     image: '/images/projects/catdetector.jpg',
+    //     date: '2015-05-15',
+    //     desc:
+    //       'Trained a convolutional neural network to classify between ~ 80 cats breeds. '
+    //       + 'to continue hosting.',
+    //   },
 ];
 
 export default data;
